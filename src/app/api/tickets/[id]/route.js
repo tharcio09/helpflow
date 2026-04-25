b294d6ab67aa38bd0f9ac05f6094d95d1df35f68
Tharcio Santos <tharcio09@hotmail.com>
2026-04-25 03:51:16 -0300

    refactor: simplifica uso do prisma e dependencias

--- src/app/api/tickets/[id]/route.js
+++ src/app/api/tickets/[id]/route.js
@@ -1,10 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../auth/[...nextauth]/route';
-import { PrismaClient } from '@prisma/client';
-
-
-const prisma = new PrismaClient();
+import prisma from '@/lib/prisma';
 
 // FUNÇÃO GET
 export async function GET(req, { params }) {
