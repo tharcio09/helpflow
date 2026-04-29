2971c3cc5946dd2bda95747f78a16d1efc561221
Tharcio Santos <tharcio09@hotmail.com>
2026-04-29 16:43:13 -0300

    test(cypress): adicionar criação dinâmica de usuários de teste

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,25 @@
-Cypress.Commands.add("getBySel", (selector, ...args) => {
-  return cy.get(`[data-cy="${selector}"]`, ...args);
+Cypress.Commands.add("createTestUser", (overrides = {}) => {
+  const uniqueId = `${Date.now()}-${Cypress._.random(1000, 9999)}`;
+  const user = {
+    name: "Usuário Teste",
+    email: `usuario-${uniqueId}@teste.com`,
+    password: "123456",
+    ...overrides,
+  };
+
+  return cy
+    .request("POST", "/api/register", {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    })
+    .then(() => user);
+});
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("/login");
+  cy.get("[data-cy='login-email']").type(email);
+  cy.get("[data-cy='login-password']").type(password);
+  cy.get("[data-cy='login-submit']").click();
+  cy.url().should("include", "/dashboard");
 });
